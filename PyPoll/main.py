6da1bbc543f7dae72f97b1572d1398406da3e247
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2024-03-30 20:53:58 -0500

    move election results to the analysis folder

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,5 @@
-#The total number of votes cast - done
-# A complete list of candidates who received votes - done*
-# The percentage of votes each candidate won -done
-# The total number of votes each candidate won - done
-# The winner of the election based on popular vote
-
 
 #### Start Here
-# create variables
-
-# read a row in the file
-# add to total votes
 
 # print the results to file
 
@@ -72,7 +62,7 @@ print(f"Winner: {winningcandidate}")
 
 
 # Print results to file
-outFilePath = "PyPoll\election_results.txt"
+outFilePath = "PyPoll\Analysis\election_results.txt"
 
 with open(outFilePath, 'w') as fileOut:
     fileOut.write("Election Results\n")
@@ -89,16 +79,3 @@ with open(outFilePath, 'w') as fileOut:
     fileOut.write(f"Winner: {winningcandidate}\n")
 
 ##### End Here
-
-# Your analysis should align with the following results:
-
-# Election Results
-# -------------------------
-# Total Votes: 369711
-# -------------------------
-# Charles Casper Stockham: 23.049% (85213)
-# Diana DeGette: 73.812% (272892)
-# Raymon Anthony Doane: 3.139% (11606)
-# -------------------------
-# Winner: Diana DeGette
-
